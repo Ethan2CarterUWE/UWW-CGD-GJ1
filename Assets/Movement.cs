e3345a3f1bee31188b01fb2d79cc6c586fd315a0
Ethan <Ethan2.Carter@live.uwe.ac.uk>
2020-10-25 02:57:55 +0000

    fixed gravity bug

---
+++ Assets/Movement.cs
@@ -0,0 +1,167 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//[RequireComponent(typeof(Rigidbody2D))]
+public class Movement : MonoBehaviour
+{
+
+    Transform transfor;
+    Rigidbody2D body;
+    public GameObject cars;
+
+    float horizontal;
+    float vertical;
+    float moveLimiter = 0.7f;
+
+    bool pause = false;
+    bool up = true;
+    bool down = false;
+    bool right = false;
+    bool left = false;
+    float y;
+
+    public float Speed = 10.0f;
+
+    void Start()
+    {
+        body = GetComponent<Rigidbody2D>();
+        y = transform.rotation.z;
+    }
+
+    void Update()
+    {
+        // Gives a value between -1 and 1
+        horizontal = Input.GetAxisRaw("Horizontal"); // -1 is left
+        vertical = Input.GetAxisRaw("Vertical"); // -1 is down
+
+        CarRotate();
+        Rotat();
+
+    }
+
+    void FixedUpdate()
+    {
+        if (horizontal != 0 && vertical != 0) // Check for diagonal movement
+        {
+            // limit movement speed diagonally, so you move at 70% speed
+            horizontal *= moveLimiter;
+            vertical *= moveLimiter;
+        }
+
+        body.velocity = new Vector2(horizontal * Speed, vertical * Speed);
+    }
+
+    void CarRotate()
+    {
+        //This doesnt work which means car atm can only face 4 directions
+        /*
+        if (right && up)
+        {
+            cars.transform.eulerAngles = new Vector3(0, 0, 135);
+            Debug.Log("pelaselord");
+
+        }
+
+        if (left && up)
+        {
+            cars.transform.eulerAngles = new Vector3(0, 0, -45);
+            Debug.Log("pelaselord");
+
+        }*/
+
+        if (right)
+        {
+            if (pause == false)
+            {
+                cars.transform.eulerAngles = new Vector3(0, 0, 90);
+
+                pause = true;
+            }
+        }
+
+
+        if (up)
+        {
+            if (pause == false)
+            {
+                cars.transform.eulerAngles = new Vector3(0, 0, 0);
+
+                pause = true;
+            }
+            //continuous rotates, need it to only do once
+        }
+
+        if (down)
+        {
+            if (pause == false)
+            {
+                cars.transform.eulerAngles = new Vector3(0, 0, 180);
+
+                pause = true;
+            }
+        }
+
+        if (left)
+        {
+            cars.transform.eulerAngles = new Vector3(0, 0, -90);
+
+            if (pause == false)
+            {
+                pause = true;
+
+
+            }
+        }
+    }
+
+    void Rotat()
+    {
+
+
+        if (vertical == 1)
+        {
+            up = true;
+        }
+        else
+        {
+            up = false;
+            pause = false;
+        }
+
+        if (vertical == -1)
+        {
+            down = true;
+        }
+        else
+        {
+            down = false;
+            pause = false;
+        }
+
+        if (horizontal == 1)
+        {
+            right = true;
+
+        }
+        else
+        {
+            right = false;
+            pause = false;
+        }
+
+        if (horizontal == -1)
+        {
+            left = true;
+
+        }
+        else
+        {
+            left = false;
+            pause = false;
+        }
+    }
+
+
+
+}
---
+++ Assets/movement2.cs
@@ -0,0 +1,117 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class movement2 : MonoBehaviour
+{
+
+    public float turnSpeed = 7;
+    private Vector3 acceleration;
+    public float maxSpeed = 0.05f;
+    public float maxReverse = 0.01f;
+
+    void Start()
+    {
+        acceleration = new Vector3(0, 0, 0);
+
+    }
+
+    void FixedUpdate()
+    {
+
+
+        //acceleration forwards
+        if (Input.GetKey(KeyCode.W))
+        {
+
+            transform.Translate(0, acceleration.y, 0);
+            if (acceleration.y <= maxSpeed)
+            {
+
+                if (acceleration.y <= maxSpeed)
+                {
+                    acceleration.y += 0.001f;
+                }
+            }
+
+        }
+
+        //brakes
+        if (Input.GetKey(KeyCode.Space))
+        {
+            transform.Translate(0, acceleration.y, 0);
+
+            if (acceleration.y >= maxReverse)
+            {
+                if (acceleration.y >= -2 * maxSpeed)
+                {
+                    acceleration.y -= 0.005f;
+                }
+            }
+        }
+
+        //rotate car
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.Rotate(Vector3.forward * turnSpeed);
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.Rotate(Vector3.forward * -turnSpeed);
+        }
+
+        Moving();
+        Debug.Log(acceleration.y);
+    }
+
+    void Moving()
+    {
+        //Function which checks player input and reduces speed if car has no acceleration .
+        //
+        if (!Input.GetKey(KeyCode.W) && !Input.GetKey(KeyCode.Space))
+        {
+            if (acceleration.y >= 0)
+            {
+                transform.Translate(0, acceleration.y, 0);
+                acceleration.y -= 0.0005f;
+            }
+            else if (acceleration.y <= 0)
+            {
+                transform.Translate(0, acceleration.y, 0);
+                acceleration.y += 0.0005f;
+
+            }
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+
+        //Function which checks how fast the car is going and depending on current speed, lower it when colliding with object
+        //
+        if (acceleration.y <= 0.1)
+        {
+            acceleration.y -= 0.03f;
+            Debug.Log("slow");
+
+        }
+        else if (acceleration.y >= 0.2f)
+        {
+            acceleration.y -= 0.1f;
+            Debug.Log("fast");
+        }
+        else
+        {
+            acceleration.y -= 0.06f;
+            Debug.Log("middle");
+
+        }
+    }
+
+
+
+
+
+
+
+}
