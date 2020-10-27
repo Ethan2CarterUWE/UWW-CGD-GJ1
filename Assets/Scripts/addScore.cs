6b72574168e17e17ce77a03937da4d885f6fcd7d
Matthew Olliff <themafu77@gmail.com>
2020-10-27 22:34:21 +0000

    Working
    Added Pick-up and drop off with score

--- Assets/Scripts/addScore.cs
+++ Assets/Scripts/addScore.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class addScore : MonoBehaviour
 {
-    public GameObject passenger;
+    public hasPassanger rider;
     public scoreScript score_script;
 
     void Start()
@@ -14,11 +14,11 @@ public class addScore : MonoBehaviour
 
     void OnTriggerEnter2D(Collider2D other)
     {
-        if (other.gameObject.CompareTag("Player"))
+        if (other.tag == "Player" & rider.rider == true)
         {
             Debug.Log("TriggerPassenger");
             score_script.current_score += 100;
-            Destroy(gameObject);
+            rider.rider = false;
         }
     }
 }
